965126176028afda7110aad1a17bdab0fc13eafc
debinov <debinv4nt@gmail.com>
2023-04-02 00:00:14 +0700

    dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+final List<String> entries = <String>['Python', 'HTML', 'C++'];
+final List<int> colorCodes = <int>[600, 500, 100];
+
 class Dashboard extends StatefulWidget{
   @override
   _DashboardState createState() => _DashboardState();  
@@ -10,19 +13,66 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context)  {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dashboard"),          
+        title: Text(
+          "Selamat Malam Username",
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Color.fromRGBO(255, 250, 250, 1),
+         ),
+        
+        ),          
       ),
       body: Column(
         children: [
-          Text("Ini di dashboard"),
+          Container(
+            child: Text(
+              "Daftar Kursus",
+              style: TextStyle(
+                fontSize: 30.0, 
+                color: Color.fromARGB(255, 33, 139, 226),
+                ),
+              
+              ),
+          ), 
+          Container(
+            child: Text(
+              "List Yang Diikuti ",
+               style: TextStyle(fontSize: 30.0,
+                color: Color.fromARGB(255, 33, 139, 226)
+                ),
+              ),
+            
+          ),
           TextButton(
             onPressed: () {
               Navigator.pushNamed(context, '/forum');
             }, 
             child: Text("Menuju forum")
-          )
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8),
+              itemCount: entries.length,
+              itemBuilder: (BuildContext context, int index){
+                return GestureDetector(
+                  child: Container(
+                    margin: EdgeInsets.all(20),
+                    color: Color.fromARGB(255, 33, 139, 226),
+                    height: 80,
+  
+                    child: Text(entries[index], 
+                    style: TextStyle(color: Color.fromARGB(255, 255, 253, 253)),),
+                  ),
+                );
+              }              
+              )
+          ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          return print("Pilih Course");
+        }),
     );
   }
 }
