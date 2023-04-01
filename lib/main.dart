330d39f7d47237da94ca22ed2aaff41784dc0253
Soviena <rovino.rs@gmail.com>
2023-04-01 23:39:37 +0700

    tambah kerangka daftar kursus

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:belajar_pro/pages/dashboard.dart';
+import 'package:belajar_pro/pages/listCourse.dart';
 import 'package:belajar_pro/pages/forum.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/dashboard',
+  initialRoute: '/listcourse',
   routes: {
     '/dashboard': (context) => Dashboard(),
     '/forum': (context) => Forum(),
+    '/listcourse': (context) => ListCourse(),
   })
 );
