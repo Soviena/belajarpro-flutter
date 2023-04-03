61ffd4c0754088117b6174bd5aadd0f2d4ec56b5
Kemaru345 <100782621+Kemaru345@users.noreply.github.com>
2023-04-03 14:37:46 +0700

    menambah warna background

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:belajar_pro/pages/dashboard.dart';
 import 'package:belajar_pro/pages/listCourse.dart';
 import 'package:belajar_pro/pages/forum.dart';
+import 'package:belajar_pro/view/splash.view.dart';
+import 'package:belajar_pro/view/login.view.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/listcourse',
-  routes: {
-    '/dashboard': (context) => Dashboard(),
-    '/forum': (context) => Forum(),
-    '/listcourse': (context) => ListCourse(),
-  })
-);
+void main() => runApp(MaterialApp(initialRoute: '/splashview', routes: {
+      '/splashview': (context) => SplashView(),
+      '/dashboard': (context) => Dashboard(),
+      '/forum': (context) => Forum(),
+      '/listcourse': (context) => ListCourse(),
+      '/login': (context) => LoginView(),
+    }));
---
+++ lib/view/Widgets/social.register.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:belajar_pro/utils/global.colors.dart';
+import 'package:belajar_pro/view/login.view.dart';
+
+class SocialRegister extends StatelessWidget {
+  const SocialRegister({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Sudah punya akun?',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(width: 5),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginView()),
+            );
+          },
+          child: Text(
+            'Masuk',
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
