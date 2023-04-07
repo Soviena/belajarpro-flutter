e4b4469172ee80c135a062d6c33cd2e748c9d6f3
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2023-04-07 16:12:26 +0700

    burgermenu dan bottomnavbar v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:belajar_pro/pages/dashboard.dart';
 import 'package:belajar_pro/pages/listCourse.dart';
 import 'package:belajar_pro/pages/forum.dart';
+import 'package:belajar_pro/pages/burgermenu.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/dashboard',
+  initialRoute: '/burgermenu',
   routes: {
     '/dashboard': (context) => Dashboard(),
     '/forum': (context) => Forum(),
     '/listcourse': (context) => ListCourse(),
+    '/burgermenu': (context) => const BurgerMenu(),
   })
 );
