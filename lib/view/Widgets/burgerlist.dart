9ca0925608fc9629b95be1a89c0751dd83d832fb
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2023-04-08 22:02:12 +0700

    burgermenu v1.0.1

--- lib/view/Widgets/burgerlist.dart
+++ lib/view/Widgets/burgerlist.dart
@@ -1,23 +1,25 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class BurgerList extends StatelessWidget {
   const BurgerList({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      width: MediaQuery.of(context).size.width,    
-      backgroundColor: Colors.black,    
+      width: MediaQuery.of(context).size.width,
+      backgroundColor: Colors.black,
       child: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
-              Color.fromARGB(100, 186, 49, 235),
-              Color.fromARGB(100, 36, 0, 255),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),          
+          Color.fromARGB(100, 186, 49, 235),
+          Color.fromARGB(100, 36, 0, 255),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         child: Column(
           children: [
             Container(
+              //tombol back
               height: 56,
               alignment: Alignment.topLeft,
               child: IconButton(
@@ -29,10 +31,33 @@ class BurgerList extends StatelessWidget {
               ),
             ),
             Container(
-              child: Expanded(
+              child: Expanded( 
                 child: ListView(
                   padding: EdgeInsets.zero,
                   children: [
+                    Positioned(
+                      left: 0,
+                      child: 
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.circle,
+                            color: Colors.blue,
+                            size: 50,
+                          ),
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text("Super namapengguna", style: TextStyle(color: Colors.white),),
+                                Text("Level 3", style: TextStyle(color: Colors.white))
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ),
                     ListTile(
                       textColor: Colors.white,
                       title: const Text('Item 1'),
