21ab4ee50778dd89bfa270172f835d61f393ecd2
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2023-04-09 11:32:16 +0700

    burgermenu v1.0.2

--- lib/view/Widgets/burgerlist.dart
+++ lib/view/Widgets/burgerlist.dart
@@ -8,7 +8,7 @@ class BurgerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      width: MediaQuery.of(context).size.width,      
+      width: MediaQuery.of(context).size.width,
       backgroundColor: Colors.black,
       child: Container(
         decoration: BoxDecoration(
@@ -18,42 +18,61 @@ class BurgerList extends StatelessWidget {
         ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         child: Column(
           children: [
-            Container(
-              //tombol back
-              height: 56,
-              alignment: Alignment.topLeft,
-              child: IconButton(
-                color: Colors.white,
-                icon: Icon(Icons.arrow_back),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  //icon back
+                  height: 56,
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    iconSize: 40,
+                    color: Colors.white,
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+                Container(
+                  //icon pesan
+                  child: IconButton(
+                    iconSize: 40,
+                    color: Colors.white,
+                    icon: Icon(Icons.mail),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
             ),
             Container(
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Icon(
-                    Icons.circle,
-                    color: Colors.blue,
-                    size: 50,
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(left: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Super namapengguna", style: TextStyle(color: Colors.white),),
-                        Text("Level 3", style: TextStyle(color: Colors.white))
-                      ],
+                margin: EdgeInsets.symmetric(horizontal: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Icon(
+                      Icons.circle,
+                      color: Colors.blue,
+                      size: 50,
                     ),
-                  ),
-                ],
-              )
-            ),
-            Expanded( 
+                    Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Super namapengguna",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text("Level 3", style: TextStyle(color: Colors.white))
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+            Expanded(
               child: ListView(
                 padding: EdgeInsets.zero,
                 children: [
